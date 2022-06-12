b9847bc1f1e604cc8478372b61d3380009af0989
Felix Titov <titow.feliks@yandex.ru>
2022-06-13 01:10:27 +0300

    Created fetching data from plist file

--- MyCarsApp/ViewController.swift
+++ MyCarsApp/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     var context: NSManagedObjectContext!
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -22,6 +22,73 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberOfTripsLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getDataFromFile()
+        
+    }
+    
+    private func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        
+        guard let pathtoFile = Bundle.main.path(forResource: "data", ofType: ".plist"),
+              let dataArray = NSArray(contentsOfFile: pathtoFile) else { return }
+        
+        for dictionary in dataArray {
+            guard let entity = NSEntityDescription.entity(forEntityName: "Car", in: context) else { return }
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String: AnyObject]
+            
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            
+            guard let imageName = carDictionary["imageName"] as? String else { return }
+            let image = UIImage(named: imageName)
+            
+            let imageData = image?.pngData()
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+            
+        }
+        
+        
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"]
+        else { return UIColor() }
+        
+        return UIColor(
+            red: CGFloat(red / 255),
+            green: CGFloat(green / 255),
+            blue: CGFloat(blue / 255),
+            alpha: 1.0
+        )
+    }
     
     @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
         
@@ -34,14 +101,5 @@ class ViewController: UIViewController {
     @IBAction func rateItPressed(_ sender: UIButton) {
         
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    }
-    
-
-
 }
 
